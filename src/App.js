99cc9a464fb2786431855ae6775caf1d62e1acc8
George Fairbairn <george.fairbairn@Georges-MacBook-Pro.local>
2019-10-02 11:58:50 -0700

    update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
+
+const SoccerPage = () => (
+  <div>
+    <h1>SOCCER PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/soccer" component={SoccerPage} />
+      </Switch>\
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -8,36 +8,41 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: "football",
-          imageUrl:
-            "http://static.nfl.com/static/content/public/pg-photo/2015/05/26/0ap3000000494154/15-maryland-s-star-spangled-banner-helmets_pg_600.jpg",
-          id: 1
-        },
-        {
           title: "soccer",
           imageUrl:
             "https://thumblr.uniid.it/blog_component/61636/20fbed293457.jpg",
-          id: 2
+          id: 1,
+          linkUrl: 'soccer'
+        },
+        {
+          title: "football",
+          imageUrl:
+            "http://static.nfl.com/static/content/public/pg-photo/2015/05/26/0ap3000000494154/15-maryland-s-star-spangled-banner-helmets_pg_600.jpg",
+          id: 2,
+          linkUrl: ''
         },
         {
           title: "ice hockey",
           imageUrl:
             "https://i.cbc.ca/1.3250417.1443629608!/fileImage/httpImage/image.JPG_gen/derivatives/4x3_1180/hockey-jerseys-chris-delorme.JPG",
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: "rugby",
           imageUrl:
             "https://media-assets-01.thedrum.com/cache/images/thedrum-prod/public-news-tmp-77017-canterbury-rwc_2015_shirt--default--1280.jpg",
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: "cricket",
           imageUrl:
             "https://sportstar.thehindu.com/cricket/article27267688.ece/ALTERNATES/LANDSCAPE_1200/GettyImages-1146184245jpg",
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         }
       ]
     };
@@ -47,8 +52,8 @@ class Directory extends React.Component {
       return (
         <div className="directory-menu">
             {
-                this.state.sections.map(({ title, imageUrl, size, id }) => 
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                this.state.sections.map(({ id, ...otherSectionProps }) => 
+                    <MenuItem key={id} {...otherSectionProps} />
                 )
             }
         </div>
